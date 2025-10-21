7fb6635f500068544b45db55edacb540d7dae74e
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-10-21 11:45:25 +0300

    deleted:    package.json 	modified:   server/config/database.js
    Deployment commit

--- server/config/database.js
+++ server/config/database.js
@@ -4,12 +4,19 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+// Connect pool to LOCALHOST
+// const pool = new Pool(
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+// });
+
+// Connect DB to Production DB usinf connectionstring
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 pool.on('connect', () => {
